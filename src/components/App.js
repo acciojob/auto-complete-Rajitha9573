0370610139915963a2a25757c5608a006b2cee83
Rajitha9573 <rajithabhukya967@gmail.com>
2024-01-26 11:07:39 +0530

    file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './../styles/App.css';
 
+const fruitsName= ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
 const App = () => {
+
+  const [fruits, setFruits] = useState();
+
+
+  const [search, setsearch] = useState("");
+
+  
+
+console.log(fruits)
+  useEffect(()=>{
+    setFruits(fruitsName.filter((code) =>
+    code.toLowerCase().includes(search.toLowerCase()))
+    
+  );
+
+  // console.log(filtered)
+  }, [search])
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <form>
+        <input value={search}  onChange={(e)=>{setsearch(e.target.value)}} />
+      </form>
+      
+
+      <ul>
+        
+        {
+        fruits && fruits.map(item =><li>{item}</li>)
+
+      }
+      </ul>
+        
+      
+
     </div>
   )
 }
